f1f5a9dd2938dd53263e9e45829d3688ad0fa3ab
Dominik Šefr <dominik.sefr@pslib.cz>
2019-11-20 09:42:28 +0100

    First Commit

---
+++ Spojovy_seznam/MujList.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Spojovy_seznam
+{
+    class MujList<T>
+    {
+        private Node FirstNode { get; set; }
+        public MujList()
+        {
+            FirstNode = null; 
+        }
+        public void Add(T v)
+        {
+            var Novy = new Node() { Value = v, Next = FirstNode };
+            FirstNode = Novy;
+        }
+        public void Remove()
+        {
+            if (FirstNode != null)
+            {
+                FirstNode = FirstNode.Next;
+            }
+        }
+
+        public void Insert(T v, int n)
+        {
+            var Help = FirstNode;
+            var pointer = 0;
+            if (n == 0)
+            {
+                Add(v);
+            }
+            else
+            {
+                while (pointer < n - 1)
+                {
+                    Help = Help.Next;
+                    pointer++;
+                }
+                var Novy = new Node() { Value = v, Next = Help.Next };
+            }
+        }
+        public override string ToString()
+        {
+            string res = "";
+            Node pointer = FirstNode;
+            while (pointer != null)
+            {
+                res += "\n" + pointer.Value.ToString();
+                pointer = pointer.Next;                
+            }
+            return res;
+        }
+        internal class Node
+        {
+            public T Value { get; set; }
+            public Node Next { get; set; }
+        }
+    }
+}
---
+++ Spojovy_seznam/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Spojovy_seznam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Seznam = new MujList<string>();
+            Seznam.Add("Ahoj");
+            Seznam.Remove();
+            Seznam.Add("Jak");
+            Seznam.Add("Se");
+            Seznam.Add("Máš");
+            Seznam.Add(".");
+            Console.WriteLine(Seznam);
+        }
+    }
+}
